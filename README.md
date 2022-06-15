5cf32c3291c2d366f29edb107ee41e5ecc2e356f
Kaspar Poland <kaspar78@mouco.com>
2022-06-15 16:38:53 -0600

    Begin documentation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # grove
 
-Grove is a proof-of-concept for subscriptable triples.
+Grove is a proof-of-concept for subscriptable triples. It might move to subscriptable shapes at some point. For now, triples is good enough, since you can subscribe to a set of them.
+
+## Usage
+
+It is a CRA application, so just `npm install` and `npm run start`, and it will run itself in `localhost:3000`.
--- src/lib/n3.ts
+++ src/lib/n3.ts
@@ -1,4 +1,4 @@
-import { Store, NamedNode, Quad, Literal, DataFactory } from "n3";
+import { Store, NamedNode, Quad, DataFactory } from "n3";
 import { SetterOrUpdater } from "recoil";
 import { quadToString } from "./utils";
 import uris from "./uris";
@@ -18,9 +18,7 @@ export interface SetterMap {
 }
 
 function updateQuadAtom(setterMap: SetterMap, quad: Quad) {
-  //   console.log("About to update QuadAtom");
   if (quadToString(quad) in setterMap) {
-    // console.log("Updating quad: ", quad);
     const updater = (curr: number) => curr + 1;
     setterMap[quadToString(quad)](updater);
   }
@@ -50,29 +48,12 @@ export const removeQuadComplex = (
   setterMap: SetterMap,
   quadToRemove: Quad
 ) => {
-  // Only remove the type quad if there aren't any other quads of that type
-  const subjectTypes = n3Store.getObjects(
+  const propertyQuad = quad(
     quadToRemove.subject,
-    uris.type,
-    quadToRemove.graph
+    uris.hasProperty,
+    quadToRemove.predicate
   );
-  const objectTypes = n3Store.getObjects(
-    quadToRemove.object,
-    uris.type,
-    quadToRemove.graph
-  );
-  if (subjectTypes.length > 0 && objectTypes.length > 0) {
-    const subjectType = subjectTypes.pop() as NamedNode;
-    const objectType = objectTypes.pop() as NamedNode; // If it has a type, it's a NamedNode
-    const typeQuad = quad(
-      subjectType,
-      quadToRemove.predicate,
-      objectType,
-      quadToRemove.graph
-    );
-
-    updateQuadAtom(setterMap, typeQuad);
-  }
+  updateQuadAtom(setterMap, propertyQuad);
 
   n3Store.removeQuad(quadToRemove);
   updateQuadAtom(setterMap, quadToRemove);
